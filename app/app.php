1adf238ebc9ef9c54f7614aade9cf5a3865a52b3
Mary Warrington <marywarrington@gmail.com>
2016-02-18 09:30:20 -0800

    Add twig and styling

--- app/app.php
+++ app/app.php
@@ -1,5 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Scrabble.php";
+
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -9,6 +11,15 @@
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array(
             // twig input associative array
+            "form"=>true, "head"=>true
+        ));
+    });
+
+    $app->post("/results", function() use ($app) {
+        $my_Scrabble = new Scrabble;
+        $result = $my_Scrabble->playScrabble($_POST['input']);
+        return $app['twig']->render('index.html.twig', array(
+            "form"=>true, "head"=>true, "message"=>array('type' => 'info', 'text' => $_POST['input'] . " is worth " . $result . " points.")
         ));
     });
 
